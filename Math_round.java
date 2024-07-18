99a2b3d254c02ab28c6a057957dda13451d15ff8
Harsh Chafle <157059028+Harshchafle@users.noreply.github.com>
2024-07-18 13:41:37 +0000

    Used Math.round() method

---
+++ Math_round.java
@@ -0,0 +1,9 @@
+/**
+ * Math_round
+ */
+public class Math_round {
+    public static void main(String[] args) {
+        System.out.println(Math.round(123.45));  // Output: 123
+        System.out.println(Math.round(-123.45)); // Output: -123
+    } 
+}
